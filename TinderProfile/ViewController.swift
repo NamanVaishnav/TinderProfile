e7316328e1f8c238b21e984927641236ef45057a
naman vaishnav <namanmvaishnav@gmail.com>
2018-11-14 00:28:46 +0530

    Initial Commit

---
+++ TinderProfile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TinderProfile
+//
+//  Created by naman vaishnav on 14/11/18.
+//  Copyright © 2018 naman vaishnav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
